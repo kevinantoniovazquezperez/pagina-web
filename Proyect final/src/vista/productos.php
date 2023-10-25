db9e5551c0a639db9353a152cdfb054e0eb158b4
DavidAlfredoVR <105959544+DavidAlfredoVR@users.noreply.github.com>
2023-10-24 19:52:37 -0600

    Update productos.php
    se agrego el código de productos php

---
+++ Proyect final/src/vista/productos.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./src/css/style_index.css">
+    <title>Arte Chiapaneca</title>
+    <title>Productos</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <!--logo (imagen que esta en la barra)-->
+          <a class="navbar-brand" href="#">
+            <img src="./src/img/logo.PNG" alt="..."  id="logo">
+          </a>
+          <!--opciones de la barra-->
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Iniciar Sesión</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Registrarse</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Productos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Conocenos</a>
+              </li>
+          </div>
+        </div>
+      </nav>
+    <header>
+      <!--la imagen debajo de las opciones-->
+      <div id="titulo">
+        <p style="font-size: 100px;">Arte Chiapaneca</p><br><br><br>
+        <p style="font-size: 25px;">¿Quieres conseguir algún producto hecho en chiapas? <br> 
+          Has encontrado el lugar correcto!!!  Ven y conoce nuestros Productos</p>
+      </div>
+    </header>
+    <br><br>
+    <h1>Artesanias de ambar</h1> <br><br>
+      <div class="container text-center, contenedores, compras">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 20rem;">
+              <a href="comprar.html">
+                <img src="../img/collar.jpg" class="card-img-top" alt="Collar" height="350px">
+              </a>
+              <div class="card-body">
+                <h5 class="card-title">Collar de ambar</h5>
+                <p class="card-text" style="text-align: justify;">Collares están confeccionados con una resina fósil de origen vegetal en forma de pequeñas bolitas para ponerla alrededor del cuello.</p><br>
+                <p class="texto">$350</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 20rem;">
+              <a href="comprar.html">
+                <img src="../img/Pulsera.jpg" class="card-img-top" alt="Collar" height="350px">
+              </a>
+              <div class="card-body">
+                <h5 class="card-title">Pulsera de ambar</h5>
+                <p class="card-text" style="text-align: justify;">Pulsera hecha a mano por un artesano o artesana, realizadas con recina fosil vegetal.</p><br><br>
+                <p class="texto">$235</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 20rem;">
+              <a href="comprar.html">
+                <img src="..//img/Aretes.jpg" class="card-img-top" alt="Collar" height="350px">
+              </a>
+              <div class="card-body">
+                <h5 class="card-title">Aretes de ambar</h5>
+                <p class="card-text" style="text-align: justify;">Aretes hechos a mano por un artesano o artesana, realizadas con recina fosil vegetal.</p> <br><br>
+                <p class="texto">$506</p>
+              </div>
+            </div>
+        </div>
+      </div>
+      <br><br>
+      <h1>Artesanias de madera</h1> <br><br>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 20rem;">
+              <a href="comprar.html">
+                <img src="../img/Carrosmadera.jpg" class="img-fluid" alt="carro de madera" height="350">
+              </a>
+              <div class="card-body">
+                <h5 class="card-title">Carro de madera</h5>
+                <p class="card-text" style="text-align: justify;">Carrito hecho de madera, perfecto para divertir a sus pequeños por horas jugando o puede servir como decoración para oficinas. Hecho a mano por artesanos mexicanos.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 20rem;">
+              <a href="comprar.html">
+                <img src="../img/Trompo.jpg" class="card-img-top" alt="Trompo" height="250">
+              </a>
+              <div class="card-body">
+                <h5 class="card-title">Trompo de madera</h5>
+                <p class="card-text" style="text-align: justify;">Es una peonza de madera a la que se hace girar por medio de una cuerda que se enrolla a su alrededor lanzándola al suelo con fuerza.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 20rem;">
+              <a href="comprar.html">
+                <img src="../img/Balero.jpg" class="card-img-top" alt="Balero" height="250">
+              </a>
+              <div class="card-body">
+                <h5 class="card-title">Balero</h5>
+                <p class="card-text" style="text-align: justify;">Está formado por un tallo largo de madera que está unido a una bola del mismo material que tiene un agujero ligeramente superior al diámetro del tallo.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br><br>
+    <footer>    <!--pie de pagina-->
+        <div class=row>
+          <div class="col">    
+            <p>Boulevard Belisario Dominguez, Kilómetro 1081, Sin Número, Teran Tuxtla Gutierrez, Chiapas.</p>
+            <p>&copy; 2020 Derechos reservado <p></p>
+      </div>
+          <div class="col">
+            <p>Contactanos</p>
+            <p>telefono: 555-555 <br> Correo: artechiapaneca@gmail.com</p>
+    
+          </div>
+        </div>
+    </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>  
+</body>
+</html>
